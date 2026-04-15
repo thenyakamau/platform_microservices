8b44b9ff3b3749c23c010e3c461ac44f6b0e9090
thenyakamau <thenyakamau@gmail.com>
2026-04-16 00:44:57 +0300

    init repository

---
+++ PlatformService/Dtos/PlatformReadDto.cs
@@ -0,0 +1,10 @@
+namespace PlatformService.Dtos
+{
+    public class PlatformReadDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Publisher { get; set; }
+        public required string Cost { get; set; }
+    }
+}
---
+++ PlatformService/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Containers typically expose only HTTP unless HTTPS is explicitly configured.
+if (!string.Equals(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"), "true", StringComparison.OrdinalIgnoreCase))
+{
+    app.UseHttpsRedirection();
+}
+PrepDb.PrepPopulation(app);
+app.MapControllers();
+
+
+app.Run();
